bfe31ec59d580464e954b44bd3bebfe83c02d244
Pawel <pawel@lowermycloudbill.com>
2020-01-08 17:27:31 -0800

    try this

--- ob-cache/test-profiles/pts/build-apache-1.5.1/pre.sh
+++ ob-cache/test-profiles/pts/build-apache-1.5.1/pre.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 rm -rf httpd/
-rm -rf httpd-2.4.7/
+rm -rf httpd-2.4.41/
 
-tar -xjvf httpd-2.4.7.tar.bz2
-mv httpd-2.4.7 httpd/
+tar -xjvf httpd-2.4.41.tar.bz2
+mv httpd-2.4.41 httpd/
 
 tar -jxvf apr-util-1.5.3.tar.bz2
 tar -jxvf apr-1.5.0.tar.bz2
